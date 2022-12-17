cc0ac9da6cb0fbd4c8b09a77cd778667795a0e1a
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-17 13:58:28 +0300

    последний вариант

--- files/grade_point_average.py
+++ files/grade_point_average.py
@@ -1,13 +1,16 @@
 file = open("check.txt")
-d = {'9': [], '10': [], '11': []}
+d = {}
 for line in file:
-    k, v = line.split()[2:4]
-    if k not in d:
-        d[k] = [v]
-    else:
-        d[k].append(v)
+    if line != '':
+        k, v = line.split()[2:]
+        if k not in d:
+            d[k] = [v]
+        else:
+            d[k].append(v)
+d = dict(sorted(d.items()))
 for val in d.values():
     a = list(map(int, val))
     b = sum(a)/len(a)
     print(b, end=' ')
+
 file.close()
