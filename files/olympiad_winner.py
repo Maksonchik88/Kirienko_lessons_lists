132d6e391dda1ec64975a2ca0340af125cad409e
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-17 23:07:33 +0300

    3_st

--- files/olympiad_winner.py
+++ files/olympiad_winner.py
@@ -2,21 +2,19 @@ file = open('check.txt')
 d = {}
 for line in file:
     a, b, c, e = line.split()
-    key = ''.join(a + " " + b)
+    key = ' '.join([a, b])
     value = int(e)
     if key not in d:
         d[key] = value
     else:
         d[key].append(int(value))
-
-print(d)
 max_v = max(d.values())
 lst = []
 for k, v in d.items():
     if v == max_v:
         lst.append(k)
 if len(lst) == 1:
-    print(lst)
+    print(*lst)
 else:
     print(len(lst))
 file.close()
