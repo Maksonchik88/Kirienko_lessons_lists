bdc4770715db81ead15d83b7c6f9ff92acc9a5ec
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-22 13:30:37 +0300

    2_nd

--- files/name_of_best_non_winner.py
+++ files/name_of_best_non_winner.py
@@ -1,22 +1,28 @@
-file = open("/home/max/projects/kirienko/files/check.txt")
+file = open("check.txt")
 d = {}
 for line in file:
     a, b, c, e = line.split()
-    name = ' '.join([a, b])
-    ball = e
-    d[name] = int(ball)
-valu = []
-for value in d.values():
-    valu.append(int(value))
-temp = sorted(valu) # [91, 92, 93, 93]
-maks = int(temp[-1])
-while maks in temp:
-    temp.remove(maks)
-gen = max(temp)
-counter = temp.count(gen)
-if counter == 1:
-    for k, v in d.items():
-        if gen == v:
-            print(k)
-else:
-    print(counter)
+    key, value = int(e), ' '.join(([a, b]))
+    if int(key) not in d:
+        d[key] = [value]
+    else:
+        d[key].append(value)
+print(d)
+max_val = 0
+pre_max = 0
+for num in d.keys():
+    if int(num) > max_val:
+        pre_max = max_val
+        max_val = num
+    elif num > pre_max:
+        pre_max = num
+print(max_val, pre_max)
+c = [d.keys()].count(pre_max)
+for k, v in d.items():
+    if k == pre_max:
+        if len(v) == 1:
+            print(*v)
+        else:
+            print(len(v))
+
+
