ea3f24166d14b94c4bf39a34183f8967397a5fd6
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-23 13:11:28 +0300

    points1

---
+++ files/sort_by_points.py
@@ -0,0 +1,26 @@
+file = open("/home/max/projects/kirienko/files/check.txt")
+d = {}
+
+for line in file:
+    a, b, c, e = line.split()
+    key = int(e)
+    value = ' '.join([a, b])
+    if key not in d:
+        d[key] = [value]
+    else:
+        d[key].append(value)
+
+x = sorted(d, reverse=True)
+
+for el in x:
+    for k, v in d.items():
+        if el == k:
+            if len(v) == 1:
+                print(*v, k, end=' ')
+                print()
+            elif len(v) > 1:
+                v.sort()
+                print(v[0], k, end=' ')
+                print()
+                print(v[1], k, end=' ')
+                print()
