4f1b2d085ad303502e01b804d95abb1aa1a63bf6
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-23 16:28:13 +0300

    alphabet2

--- files/sort_participants_alphabetically.py
+++ files/sort_participants_alphabetically.py
@@ -6,12 +6,10 @@ for line in file:
     value = int(e)
     if key not in d:
         d[key] = value
-print(d)
+
 x = list(sorted(d))
-print(x)
+
 for el in x:
-    for k, v in d.items():
-        if el == k:
-            print(k, v)
+    print(el, d[el])
 
 file.close()
