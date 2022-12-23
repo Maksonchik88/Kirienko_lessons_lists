2f3798c96eb25f9eaca74bee7d1295f62a8bc580
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-23 16:05:11 +0300

    smallest2

--- files/school_smallest_participans.py
+++ files/school_smallest_participans.py
@@ -3,22 +3,24 @@ d = {}
 file = open('check.txt')
 for line in file:
     school, ball = list(map(int, line.split()[-2:]))
-    if school in d:
-        d[school][0] += 1
-        d[school][1] += ball
+    if d.get(school, None):
+        d[school] += 1
     else:
-        d[school] = [1, ball]
-print(d) # {14: [2, 131], 23: [1, 74], 3: [2, 155], 27: [1, 68]}
+        d[school] = 1
+
 min_score = 100
 for c in d.values():
-    if c[0] < min_score:
-        min_score = c[0]
-print(min_score)
-d_list = sorted(d.items(), reverse=False)
-print(d_list)
-for i in d_list:
-    for j in i[1]:
-        if j == min_score:
-            print(i[0], end=' ')
+    if c < min_score:
+        min_score = c
+
+d2 = {}
+for k, v in d.items():
+    if v not in d2:
+        d2[v] = [k]
+    else:
+        d2[v].append(k)
+
+print(*sorted(d2[min(d2)]))
+
 
 file.close()
