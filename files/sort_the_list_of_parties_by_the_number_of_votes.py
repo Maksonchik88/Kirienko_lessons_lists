243cbd7abca4143f01ecb4322fffd382f015a3e3
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-29 18:03:06 +0300

    Упорядочить список партий по числу голосов
    * Открыл файл -> прочитал его целиком  -> закрыл файл
    * Удалил в конце каждой строки символ переноса строки
    * Созда список названий компаний и список голосов
    * Инициализировал словарь, где ключами являются названия партий, а ключами 0
    * Заполнил значения ключей словаря количествами голосов за каждую партию
    * Создали список для случайных голосов
    * Создал новый словарь, где ключи -  это количества голосов, а значения - списки партий
    * Отсортировал ключи - количества голосов по убыванию
    * Циклом вывел отсортированные названия партий и вывел на печать (с ошибкойб переделаю в следующем коммите)

--- files/sort_the_list_of_parties_by_the_number_of_votes.py
+++ files/sort_the_list_of_parties_by_the_number_of_votes.py
@@ -3,26 +3,29 @@ raw_text = file.readlines()
 
 file.close()
 
-processed_text = list(filter(None, (line.rstrip() for line in raw_text)))
+processed_text = list((line.rstrip() for line in raw_text))
+# processed_text = list(filter(None, (line.rstrip() for line in raw_text)))
 print(processed_text)
 parties_list = processed_text[processed_text.index("PARTIES:") + 1: processed_text.index("VOTES:")]
 votes_list = processed_text[processed_text.index("VOTES:") + 1:]
-print(parties_list)
-print(votes_list)
+
 data = dict.fromkeys(parties_list, 0)
-print(data)
+error_list = []
 for voice in votes_list:
-    data[voice] += 1
-print(data)
+    if voice not in data:
+        error_list.append(voice)
+    else:
+        data[voice] += 1
+
 revers_data = {}
 for party, counter in data.items():
     if counter not in revers_data:
         revers_data[counter] = [party]
     else:
         revers_data[counter].append(party)
-print(revers_data)
+
 count_sort = sorted(revers_data.keys(), reverse=True)
-print(count_sort)
+
 for num in count_sort:
     a = revers_data[num]
     a.sort()
