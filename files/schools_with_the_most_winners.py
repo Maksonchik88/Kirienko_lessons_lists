d0448e6cfc6f0d3a4eb4e2506f7ad1de316f10eb
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-29 11:46:27 +0300

    *Инициализировал и заполнил словарь, в котором ключом является количество участниковБ а значиением - список школ. *Нашел максимум количества победителей *Отсортировал и вывел школу(школы) в порядке возрастания номера школы с максимальным количеством победителей

--- files/schools_with_the_most_winners.py
+++ files/schools_with_the_most_winners.py
@@ -5,12 +5,12 @@ max_score = 0
 for line in file:
     _, _, school, score = line.split()
     school, score = int(school), int(score)
+    if score >= max_score:
+        max_score = score
     if school not in data:
         data[school] = [score]
     else:
         data[school].append(score)
-    if score > max_score:
-        max_score = score
 
 file.close()
 
@@ -20,9 +20,17 @@ for school, scores in data.items():
     for score in scores:
         if score == max_score:
             counter += 1
-    data_count_of_winners[school] = int(counter)
+    data_count_of_winners[school] = counter
 
-max_school = (max(data_count_of_winners, key=data_count_of_winners.get))
+rev_data_count_of_winners = {}
+for school, score in data_count_of_winners.items():
+    if score not in rev_data_count_of_winners:
+        rev_data_count_of_winners[score] = [school]
+    else:
+       rev_data_count_of_winners[score].append(school)
 
-print(max_school)
+max_count = max(rev_data_count_of_winners.keys())
 
+schools_enter = sorted(rev_data_count_of_winners[max_count])
+print(*schools_enter)
+# max_school = (max(data_count_of_winners, key=data_count_of_winners.get))
