7c7aa287be182611b62caa113039c1e955ce1e46
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-29 18:31:43 +0300

    * Убрал filter() * Добавил в цикл, где формируется глобальная переменная условие, которое исключит ошибку в случае, если будет "испорченная бюллетень")

--- files/seven_percent_barrie.py
+++ files/seven_percent_barrie.py
@@ -3,7 +3,7 @@ lines = file.readlines()
 
 file.close()
 
-lines = list(filter(None, (line.rstrip() for line in lines)))
+lines = list((line.rstrip() for line in lines))
 
 parties_list = lines[lines.index("PARTIES:") + 1: lines.index("VOTES:")]
 votes_list = lines[lines.index("VOTES:") + 1:]
@@ -12,8 +12,11 @@ data_parties_keys = dict.fromkeys(parties_list, 0)
 
 global_votes = 0
 for voice in votes_list:
-    global_votes += 1
-    data_parties_keys[voice] += 1
+    if voice in data_parties_keys:
+        global_votes += 1
+        data_parties_keys[voice] += 1
+    else:
+        continue
 
 for party, votes in data_parties_keys.items():
     if votes >= 7 * global_votes / 100:
