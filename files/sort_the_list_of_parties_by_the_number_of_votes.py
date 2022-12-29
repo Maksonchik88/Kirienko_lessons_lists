49e3738a304db976a4a99c1b48467f834f06c3cd
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-29 18:21:58 +0300

    * Убрал список ошибочных голосов и просто пропускаю в цикле случаи, когда будет ошибочный голос * Через цикл вывел построчно названия партий согласно условию задачи

--- files/sort_the_list_of_parties_by_the_number_of_votes.py
+++ files/sort_the_list_of_parties_by_the_number_of_votes.py
@@ -4,16 +4,15 @@ raw_text = file.readlines()
 file.close()
 
 processed_text = list((line.rstrip() for line in raw_text))
-# processed_text = list(filter(None, (line.rstrip() for line in raw_text)))
+# processed_text = list(filter(None, (line.rstrip() for line in raw_text))
 print(processed_text)
 parties_list = processed_text[processed_text.index("PARTIES:") + 1: processed_text.index("VOTES:")]
 votes_list = processed_text[processed_text.index("VOTES:") + 1:]
 
 data = dict.fromkeys(parties_list, 0)
-error_list = []
 for voice in votes_list:
     if voice not in data:
-        error_list.append(voice)
+        continue
     else:
         data[voice] += 1
 
@@ -27,6 +26,7 @@ for party, counter in data.items():
 count_sort = sorted(revers_data.keys(), reverse=True)
 
 for num in count_sort:
-    a = revers_data[num]
-    a.sort()
-    print(*a)
+     parties_lst = revers_data[num]
+     parties_lst.sort()
+     for party in parties_lst:
+         print(party)
