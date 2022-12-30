f37c1ebaa98a6e7c62903bf16cdff95637cf48ad
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-30 18:11:21 +0300

    * Использовал агрегацию при инициализации и заполнении нового словаря, в котором ключ - голоса, значения - список кандидатов * Использовал с помощью цикла максимальное колическтиов голосов и предмаксимальное * Инициализировал переменную, значением которой является формула вычисления в процентном соотношении количества голосов по каждому канидаьу * С помощью условных операторов вывел результат

--- files/presedent_elections.py
+++ files/presedent_elections.py
@@ -4,29 +4,35 @@ candidates = file.readlines()
 file.close()
 
 data = {}  # {'Полуэкт Варфоломеев': 2, 'Варфоломей Виссарионов': 3, 'Виссарион Полуэктов': 1}
-total_voces = 0  # 6
+total_votes = 0  # 6
 for candidat in candidates:
     candidat = ' '.join(candidat.split())
-    total_voces += 1
+    total_votes += 1
     if candidat not in data:
         data[candidat] = 0
-        data[candidat] += 1
-    else:
-        data[candidat] += 1
+    data[candidat] += 1
 
 votes_list = sorted(data.values(), reverse=True)  # [3, 2, 1]
 
-data_rev = {}  # {'Варфоломей Виссарионов': 3, 'Полуэкт Варфоломеев': 2, 'Виссарион Полуэктов': 1}
+data_rev = {}  # {2: ['Полуэкт Варфоломеев'], 3: ['Варфоломей Виссарионов'], 1: ['Виссарион Полуэктов']}
 
+for fullname, votes in data.items():
+    if votes not in data_rev:
+        data_rev[votes] = []
+    data_rev[votes].append(fullname)
 
-for num in votes_list:
-    for fullname, votes in data.items():
-        if num == votes:
-            data_rev[fullname] = num
-data_tuple = list(data_rev.items())  # [('Варфоломей Виссарионов', 3), ('Полуэкт Варфоломеев', 2), ('Виссарион Полуэктов', 1)]
+first_place = 0
+second_place = 0
+for votes, fullname in data_rev.items():
+    if first_place < votes:
+        second_place = first_place
+        first_place = votes
+    elif second_place < votes:
+        second_place = votes
 
+formula = (100 * first_place / total_votes)
 
-if (100 * data_tuple[0][1])/total_voces > 50:
-    print(data_tuple[0][0])
-elif (100 * data_tuple[0][1])/total_voces <= 50:
-    print(data_tuple[0][0], data_tuple[1][0], sep='\n')
+if formula > 50:
+    print(*data_rev[first_place])
+else:
+    print(*data_rev[first_place], *data_rev[second_place], sep='\n')
