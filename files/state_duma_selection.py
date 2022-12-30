c5e6e6c257c8305ee38d628e65835580f2d49f77
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-30 12:09:16 +0300

    Выборы Государственной Думы
    https://informatics.msk.ru/mod/statements/view.php?id=5763&chapterid=111357#1
    *Открыл файл -> Прочитал файл целиком  - > Закрыл файл (закрою)))
    *Инициализировал словарь и заполнил его ключами (названия партий) и значениями (количеством голосов)
    *Инициализировал переменную общей суммы всех голосов
    *Инициализировал и посчитал "первое избирательное частное"
    *Инициализировал и заполнил словарь, в котором ключами являются названия партий, а значениями - списки, состоящие из количества мест в Думу,
         дробной части частного от деления числа голосов за данную партию на первое избирательное частное
    *Возникла проблема с дальнейшим присвоением дополнительного балла партииБ согласно условию задачи

---
+++ files/state_duma_selection.py
@@ -0,0 +1,27 @@
+file = open("files/check.txt")
+text = file.readlines()
+data = {}  # {'Party One': [100000], 'Party Two': [200000], 'Party Three': [400000]}
+constanta = 450
+total_votes = 0  # 700000
+for line in text:
+    party, number, votes = line.split()
+    votes = int(votes)
+    party_name = ' '.join([party, number])
+    data[party_name] = votes
+    total_votes += votes
+
+FIP = total_votes / constanta  # 1555.5555555555557
+
+number_of_seats = {}  # {'Party One': [64, 0.2857142857142918], 'Party Two': [128, 0.5714285714285836], 'Party Three': [257, 0.14285714285716722]}
+for party, number in data.items():
+    temp = number / FIP
+    remainder = (number * constanta) / total_votes
+    remainder = remainder - int(remainder)
+    number_of_seats[party] = [int(temp)]
+    number_of_seats[party].append(remainder)
+
+for party, numbers_list in number_of_seats.items():
+    for position in numbers_list:
+        passing_score = total_votes / FIP
+        if passing_score <= constanta:
+            pass
