7103400c0474d8574afcb18723b5ac5d172c829b
Maksim Kolesnikov <89533464344maks@gmail.com>
2022-12-30 12:48:45 +0300

    * Удалил сортировку с lambda-функцией, использовал вместо этого цикл

--- files/presedent_elections.py
+++ files/presedent_elections.py
@@ -3,8 +3,8 @@ candidates = file.readlines()
 
 file.close()
 
-data = {}
-total_voces = 0
+data = {}  # {'Полуэкт Варфоломеев': 2, 'Варфоломей Виссарионов': 3, 'Виссарион Полуэктов': 1}
+total_voces = 0  # 6
 for candidat in candidates:
     candidat = ' '.join(candidat.split())
     total_voces += 1
@@ -14,7 +14,17 @@ for candidat in candidates:
     else:
         data[candidat] += 1
 
-data_tuple = sorted(data.items(), key=lambda x: x[1], reverse=True)
+votes_list = sorted(data.values(), reverse=True)  # [3, 2, 1]
+
+data_rev = {}  # {'Варфоломей Виссарионов': 3, 'Полуэкт Варфоломеев': 2, 'Виссарион Полуэктов': 1}
+
+
+for num in votes_list:
+    for fullname, votes in data.items():
+        if num == votes:
+            data_rev[fullname] = num
+data_tuple = list(data_rev.items())  # [('Варфоломей Виссарионов', 3), ('Полуэкт Варфоломеев', 2), ('Виссарион Полуэктов', 1)]
+
 
 if (100 * data_tuple[0][1])/total_voces > 50:
     print(data_tuple[0][0])
