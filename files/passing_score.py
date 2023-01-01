c174caa789d55598cb06262faf53ea8c62871f26
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-01 17:38:22 +0300

    Проходной балл
    *Открыл файл
    *Инициализировал переменную из первой строки файла, в к-й хранится кол-во мест
    *Прочитал все остальные строки файла -> закрвл файл
    *Инициализировал два словаря и сортировкой заполнил словари, соблюдая условие, что если хоть один предмет сдан менее, чем на 40 баллов,
        то это в словарь непрозодных абитуриентов, а если больше -словарь проходных. Ключами в словарях являются средние баллы,
        а значениями - списки абитуриентов
    *Отсортировал словари по нисходящей по ключу
    *Инициализировал и вычислил через цикл максимальную длину значений в проходном словаре
    *Проверил, не меньше ли общее число абитуриентов, прошедших первый этап числа месте. Если нет, то дальше
    *Циклом вычислил проходной бал, вывел его

---
+++ files/passing_score.py
@@ -0,0 +1,50 @@
+file = open("files/check.txt")
+num_of_sets = int(file.readline())
+lines = file.readlines()
+
+file.close()
+
+scores_of_all_university_applicants = {}
+did_not_pass_the_competition = {}
+total_applicants = 0
+total_did_not_applicants = 0
+for line in lines:
+        line_for_sorting = line.split()
+        balls = list(map(int, line_for_sorting[-3:]))
+        fullname = ' '.join(line_for_sorting[:-3])
+        sum_balls = sum(balls)
+        min_balls = min(balls)
+        if min_balls < 40:
+                total_did_not_applicants += 1
+                if sum_balls not in did_not_pass_the_competition:
+                        did_not_pass_the_competition[sum_balls] = []
+                did_not_pass_the_competition[sum(balls)].append(fullname)
+        elif min_balls >= 40:
+                total_applicants += 1
+                if sum_balls not in scores_of_all_university_applicants:
+                        scores_of_all_university_applicants[sum_balls] = []
+                scores_of_all_university_applicants[sum_balls].append(fullname)
+
+scores_of_all_university_applicants_sort = dict(sorted(scores_of_all_university_applicants.items(), reverse=True))
+did_not_pass_the_competition_sort = dict(sorted(did_not_pass_the_competition.items(), reverse=True))
+print(scores_of_all_university_applicants_sort)
+print(did_not_pass_the_competition_sort)
+print(total_did_not_applicants, total_applicants)
+
+max_len_of_fullnames = 0
+for ball, fulnames in scores_of_all_university_applicants_sort.items():
+        if len(fulnames) > max_len_of_fullnames:
+                max_len_of_fullnames = len(fulnames)
+print(max_len_of_fullnames)
+
+if total_applicants <= num_of_sets:
+        print(0)
+elif max_len_of_fullnames > num_of_sets:
+        print(1)
+
+for ball, fulnames in scores_of_all_university_applicants_sort.items():
+        if num_of_sets - 1 <= len(fulnames) < num_of_sets:
+                print(ball)
+                break
+        else:
+                num_of_sets -= len(fulnames)
