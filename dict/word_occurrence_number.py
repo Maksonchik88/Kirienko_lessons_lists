10b52bbc49f8d3c421030ef262a3478870b971ee
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-06 09:12:52 +0300

    Номер появления слова
    *Инициализировал словарь
    *Открыл и прочитал файл
    *Заполнил словарь слово -> количесво повторений, распечатывая при этом каждое значение в итерации

---
+++ dict/word_occurrence_number.py
@@ -0,0 +1,7 @@
+data = {}
+with open("dict/text.txt") as file:
+    for lines in file:
+        lines = lines.split()
+        for line in lines:
+            data[line] = data.get(line, -1) + 1
+            print(data[line], end=' ')
