fe5d7ede6394c1c6413ec8589e33bd24e2319408
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-08 13:10:24 +0300

    альтернатив без гетт

--- dict/word_occurrence_number.py
+++ dict/word_occurrence_number.py
@@ -4,6 +4,13 @@ with open("dict/text.txt") as file:
     for lines in file:
         lines = lines.split()
         for line in lines:
-            data[line] = data.get(line, -1)
-            data[line] += 1
-            print(data[line], end=' ')
+            if line not in data:
+                data[line] = 0
+                print(data[line], end=' ')
+            else:
+                data[line] += 1
+                print(data[line], end=' ')
+
+            # data[line] = data.get(line, -1)
+            # data[line] += 1
+            # print(data[line], end=' ')
