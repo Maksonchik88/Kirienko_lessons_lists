8dca6039065e981a554151c02297f5012cf947a7
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-08 11:49:53 +0300

    https://informatics.msk.ru/mod/statements/view.php?id=4535&chapterid=3759#1
    *Описал алгоритм присвоения значения с помощью метода get() в две строки для наглядности
    *Коснтанта -1 задает начальное значение по ключу в случае, когда мы инициализируем ключ -> значение в словаре
    *Следующим действием мы добавляем к текущему значению единицу и записываем в словрь это значение
    *В случае, когда ключ в словаре присутвует, get() возвращает существующее значение, а затем увеличивает счетчик на единицу

--- dict/word_occurrence_number.py
+++ dict/word_occurrence_number.py
@@ -3,5 +3,6 @@ with open("dict/text.txt") as file:
     for lines in file:
         lines = lines.split()
         for line in lines:
-            data[line] = data.get(line, -1) + 1
+            data[line] = data.get(line, -1)
+            data[line] += 1
             print(data[line], end=' ')
