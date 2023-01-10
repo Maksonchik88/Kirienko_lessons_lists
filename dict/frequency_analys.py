2b073cc44a8b4cce7fb7e1954551bdc7943aca5f
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-10 19:06:56 +0300

    Частотный анализ
    https://informatics.msk.ru/mod/statements/view.php?id=4535&chapterid=3764#1
    *Открыл файл -> прочитал  -> закрыл файл
    *Инициализировал словарь, заполнив его слово -> количество повторений
    *Вывел, согласно сортировке, указанной в задании

---
+++ dict/frequency_analys.py
@@ -0,0 +1,18 @@
+file = open("dict/text.txt")
+lines = file.readlines()
+
+file.close()
+
+data = {}
+
+for line in lines:
+    line = line.split()
+    for word in line:
+        if word not in data:
+            data[word] = 1
+        data[word] += 1
+print(data)
+data_sort = sorted(data.items(), key=lambda x: [-x[1], x[0]])
+print(data_sort)
+for word, count in data_sort:
+    print(word)
