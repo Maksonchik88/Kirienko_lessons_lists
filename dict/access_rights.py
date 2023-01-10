3823e9231d433f6ba5d5e3efabc7712f18d58866
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-10 19:45:17 +0300

    *Переделал, исходя из рекомендаций

--- dict/access_rights.py
+++ dict/access_rights.py
@@ -7,21 +7,20 @@ print(action_on_a_file)
 file = open("dict/text.txt")
 count = int(file.readline())
 data = {}
-lines = file.readlines()[:count]
 
-for line in lines:
+for _ in range(count):
+    line = file.readline()
     line_for_sort = line.split()
     name = line_for_sort[0]
     options = line_for_sort[1:]
     data[name] = options
 
 count_2 = int(file.readline())
-check_lines = file.readlines()[count_2:]
-
-file.close()
-
+data_check = {}
+check_lines = file.readlines()
 for line in check_lines:
     action, file = line.split()
+    data_check[file] = action
     if action_on_a_file[action] in data[file]:
         print("OK")
     else:
