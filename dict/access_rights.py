e18e6e150d64d89b912d1de13d90968a81b70eb3
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-11 08:13:49 +0300

    Действительно, а зачем?)

--- dict/access_rights.py
+++ dict/access_rights.py
@@ -3,11 +3,10 @@ action_on_a_file = {
     'write': 'W',
     'execute': 'X'
 }
-print(action_on_a_file)
 file = open("dict/text.txt")
+
 count = int(file.readline())
 data = {}
-
 for _ in range(count):
     line = file.readline()
     line_for_sort = line.split()
@@ -16,11 +15,9 @@ for _ in range(count):
     data[name] = options
 
 count_2 = int(file.readline())
-data_check = {}
 check_lines = file.readlines()
 for line in check_lines:
     action, file = line.split()
-    data_check[file] = action
     if action_on_a_file[action] in data[file]:
         print("OK")
     else:
