4c1760d4298cbc91d8fffd189ea83180248fed64
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-11 19:39:28 +0300

    Отсортировал фрукты в последнем цикле

--- dict/english_latin_dictionary.py
+++ dict/english_latin_dictionary.py
@@ -14,11 +14,10 @@ for line in lines:
         else:
             en_data[mean].append(english)
 
-# en_data_sort = sorted(sorted(en_data.items(), key=lambda x: len(x[0])), key=lambda x: x[0])
 en_data_sort = sorted(en_data.items())
 
 for latin, english in en_data_sort:
     if len(english) == 1:
         print(latin, '-', *english)
     else:
-        print(latin, '-', ', '.join(english))
+        print(latin, '-', ', '.join(sorted(english)))
