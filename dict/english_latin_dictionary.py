388ce55d736b1a2437650729454dbc1e1b9bb915
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-11 18:20:37 +0300

    Англо-латинский словарь
    https://informatics.msk.ru/mod/statements/view.php?id=4535&chapterid=3767#1
    
    *Открыл файл -> прочитал -> забыл закрыть
    *Инициализировал словарь, где английское слово -> список латинских слов
    *Отсортировал словарь
    *Вывел результат
    
    p.s.
    *не убрал принты, вижу
    *воспользовался впервые двойной сортировкой. Был ли смысл?
    *восполозовался  "*" в цикле. Как я понимаю, это "запаковка"?
    *решением недоволен, сумбур какой-то

---
+++ dict/english_latin_dictionary.py
@@ -0,0 +1,20 @@
+file = open("dict/text.txt")
+file.readline()
+lines = file.readlines()
+en_data = {}
+for line in lines:
+    line = line.replace(' -', '').rstrip().replace(',', '')
+    english, *latin = line.split()
+    for mean in latin:
+        if mean not in en_data:
+            en_data[mean] = [english]
+        else:
+            en_data[mean].append(english)
+print(en_data)
+en_data_sort = sorted(sorted(en_data.items(), key=lambda x: len(x[0])), key=lambda x: x[0])
+print(en_data_sort)
+for latin, english in en_data_sort:
+    if len(english) == 1:
+        print(latin, '-', *english)
+    else:
+        print(latin, '-', ', '.join(english))
