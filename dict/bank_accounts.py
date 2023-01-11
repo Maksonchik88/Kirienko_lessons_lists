52c9438008be5302d8a4e430b5c15bef28ecbd2b
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-11 11:47:06 +0300

    Банковские счета
    https://informatics.msk.ru/mod/statements/view.php?id=4535&chapterid=3766#1
    
    *Открыл файл -> прочитал  -> закрыл файл
    *Инициаоизировал словарь
    *Заполнил его, согласно условию
    
    P.S. почему в задаче толоько раз выводится 105, если у нас по ходу решения это число выводится дважды?

---
+++ dict/bank_accounts.py
@@ -0,0 +1,41 @@
+file = open("dict/text.txt")
+lines = file.readlines()
+file.close()
+bank_accounts = {}
+
+for line in lines:
+    if line.startswith('DEPOSIT'):
+        _, name, amount = line.split()
+        amount = int(amount)
+        if name not in bank_accounts:
+            bank_accounts[name] = amount
+        else:
+            bank_accounts[name] += amount
+    if line.startswith('WITHDRAW'):
+        _, name, amount = line.split()
+        amount = int(amount)
+        if name not in bank_accounts:
+            bank_accounts[name] = 0
+        bank_accounts[name] -= amount
+    if line.startswith('BALANCE'):
+        _, name = line.split()
+        if name not in bank_accounts:
+            print('ERROR')
+        else:
+            print(bank_accounts[name])
+    if line.startswith('TRANSFER'):
+        _, name_from, name_to, amount = line.split()
+        amount = int(amount)
+        if name_from not in bank_accounts:
+            bank_accounts[name_from] = 0
+        bank_accounts[name_from] -= amount
+        if name_to not in bank_accounts:
+            bank_accounts[name_to] = 0
+        bank_accounts[name_to] += amount
+    if line.startswith('INCOME'):
+        _, balance = line.split()
+        balance = int(balance)
+        for name, amount in bank_accounts.items():
+            if amount > 0:
+                bank_accounts[name] += (amount * balance / 100)
+                print(bank_accounts[name])
