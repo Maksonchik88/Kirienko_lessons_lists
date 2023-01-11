5c218d2aa01d6957c5ee2e06c0dbdcd591f8fd85
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-11 08:47:51 +0300

    Переделал

--- dict/frequency_analys.py
+++ dict/frequency_analys.py
@@ -4,15 +4,14 @@ lines = file.readlines()
 file.close()
 
 data = {}
-
 for line in lines:
     line = line.split()
     for word in line:
         if word not in data:
-            data[word] = 1
+            data[word] = 0
         data[word] += 1
-print(data)
-data_sort = sorted(data.items(), key=lambda x: [-x[1], x[0]])
-print(data_sort)
+
+data_sort = sorted(data.items(), key=lambda x: (x[1], x[0]), reverse=True)
+
 for word, count in data_sort:
     print(word)
