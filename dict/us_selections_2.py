7c85c4e0789c1de01b3257922015d320f0d3ef6d
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-01-23 15:32:07 +0300

    пусто

--- dict/us_selections_2.py
+++ dict/us_selections_2.py
@@ -7,7 +7,7 @@ for line in range(number_of_states):
     number_of_electors = int(number_of_electors)
     if state_name not in states_and_electors:
         states_and_electors[state_name] = number_of_electors
-
+print(states_and_electors)
 end_part_of_file = file.readlines()
 
 file.close()
@@ -23,6 +23,8 @@ print(results_for_each_participant)
 candidates_list = []
 for state, candidate_count in results_for_each_participant.items():
     candidate_count_sort = sorted(candidate_count.items(), key=lambda x: (-x[1], x[0]))
+    print(candidate_count_sort)
     print(candidate_count_sort[0][0], states_and_electors[state])
     candidates_list.append(candidate_count_sort[0][0])
 print(candidates_list)
+total_votes = {}
